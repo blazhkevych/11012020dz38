b3ad5147126fcafa1373935ab1400aa3c280f112
blazhkevych <65856963+blazhkevych@users.noreply.github.com>
2020-07-19 18:48:53 +0300

    Добавьте файлы проекта.

---
+++ 11012020dz38.cpp
@@ -0,0 +1,15 @@
+﻿/*
+* Пользователь 	вводит с клавиатуры объем в гигабайтах.Перевести его в биты.
+*/
+#include <iostream>
+#include <Windows.h>;
+using namespace std;
+int main()
+{
+	SetConsoleOutputCP(1251);
+	cout << "Введите объем в гигабайтах : \n";
+	int a;
+	cin >> a;
+	cout << "бит = " << a * 8589934592 << endl;
+	return 0;
+}
